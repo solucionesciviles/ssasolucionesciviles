5714cea04057bb4627ff3c50b4595aaa16a1ee0f
ssascg <stabaref1973@outlook.com>
2025-03-16 14:32:59 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -9,29 +9,31 @@ document.addEventListener('DOMContentLoaded', () => {
             navBar.classList.remove('scrolled');
         }
     });
-// Mostrar los botones cuando se hace scroll hacia abajo
-window.onscroll = function() {
-    var backToTopButton = document.getElementById("back-to-top");
-    var goBackButton = document.getElementById("go-back");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        backToTopButton.style.display = "block";
-        goBackButton.style.display = "block";
-    } else {
-        backToTopButton.style.display = "none";
-        goBackButton.style.display = "none";
-    }
-};
 
-// Volver al inicio cuando se hace clic en el botón "Volver arriba"
-document.getElementById("back-to-top").onclick = function() {
-    document.body.scrollTop = 0; // Para Safari
-    document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
-};
+    // Mostrar los botones cuando se hace scroll hacia abajo
+    window.onscroll = function() {
+        var backToTopButton = document.getElementById("back-to-top");
+        var goBackButton = document.getElementById("go-back");
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            backToTopButton.style.display = "block";
+            goBackButton.style.display = "block";
+        } else {
+            backToTopButton.style.display = "none";
+            goBackButton.style.display = "none";
+        }
+    };
 
-// Volver a la página de servicios cuando se hace clic en el botón "Volver a Servicios"
-document.getElementById("go-back").onclick = function() {
-    window.location.href = 'services.html'; // Redirigir a la página de servicios
-};
+    // Volver al inicio cuando se hace clic en el botón "Volver arriba"
+    document.getElementById("back-to-top").onclick = function() {
+        document.body.scrollTop = 0; // Para Safari
+        document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
+    };
+
+    // Volver a la página de servicios cuando se hace clic en el botón "Volver a Servicios"
+    document.getElementById("go-back").onclick = function() {
+        window.location.href = 'services.html'; // Redirigir a la página de servicios
+    };
+
     // Validación del formulario de contacto
     const contactForm = document.querySelector('#contact-form form');
     if (contactForm) {
