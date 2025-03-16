7de8ed4c9201d379830b766610c111fcc95e423d
ssascg <stabaref1973@outlook.com>
2025-03-16 12:00:24 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -28,40 +28,9 @@ document.getElementById("back-to-top").onclick = function() {
     document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
 };
 
-// Volver atrás o a la página de inicio cuando se hace clic en el botón "Volver atrás o a Inicio"
+// Volver a la página de servicios cuando se hace clic en el botón "Volver a Servicios"
 document.getElementById("go-back").onclick = function() {
-    if (document.referrer) {
-        window.history.back(); // Volver a la página anterior si hay una
-    } else {
-        window.location.href = 'index.html'; // Ir a la página de inicio si no hay una página anterior
-    }
-};
-// Mostrar los botones cuando se hace scroll hacia abajo
-window.onscroll = function() {
-    var backToTopButton = document.getElementById("back-to-top");
-    var goBackButton = document.getElementById("go-back");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        backToTopButton.style.display = "block";
-        goBackButton.style.display = "block";
-    } else {
-        backToTopButton.style.display = "none";
-        goBackButton.style.display = "none";
-    }
-};
-
-// Volver al inicio cuando se hace clic en el botón "Volver arriba"
-document.getElementById("back-to-top").onclick = function() {
-    document.body.scrollTop = 0; // Para Safari
-    document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
-};
-
-// Volver atrás o a la página de inicio cuando se hace clic en el botón "Volver atrás o a Inicio"
-document.getElementById("go-back").onclick = function() {
-    if (document.referrer) {
-        window.history.back(); // Volver a la página anterior si hay una
-    } else {
-        window.location.href = 'index.html'; // Ir a la página de inicio si no hay una página anterior
-    }
+    window.location.href = 'services.html'; // Redirigir a la página de servicios
 };
     // Validación del formulario de contacto
     const contactForm = document.querySelector('#contact-form form');
